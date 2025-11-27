7e45441e3f06db91c0f1eb3c343cd5836931215b
Student <haliliartur894@gmail.com>
2025-11-27 16:15:37 +0100

    m

---
+++ module 8 oop/access_modifiers/protected_modifiers.py
@@ -0,0 +1,13 @@
+class MyClass:
+    def __init__(self):
+        self.__protected_variable = "This is a protected variable"
+
+    def __private_method(self):
+        print("This is a protected method")
+
+
+my_class = MyClass()
+
+print(my_class.__protected_variable)
+print(my_class.__protected_method())
+
